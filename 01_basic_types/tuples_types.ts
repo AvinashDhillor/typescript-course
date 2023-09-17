c481368202ea33f2843bd4b523af8430f1f15bd8
Avinash Dhillor <avinashdhillor24@gmail.com>
2023-09-17 21:35:19 +0530

    commited by Avinash Dhillor on 2023-09-17

---
+++ 01_basic_types/tuples_types.ts
@@ -0,0 +1,7 @@
+const person1 : {
+    role : [string, string] // example of tuple. Only two elements can be stored in array and both will be string.
+} = {
+    role: ["admin", "dev"]
+}
+
+console.log(person1.role);
